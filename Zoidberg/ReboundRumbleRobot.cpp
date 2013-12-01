061c05a9d853b7ba7d60bc26508d10f599d02fb6
ratpack <scottemorton@comcast.net>
2013-12-01 14:04:19 -0500

    got drivetrain working

--- Zoidberg/ReboundRumbleRobot.cpp
+++ Zoidberg/ReboundRumbleRobot.cpp
@@ -1,10 +1,17 @@
 #include "WPILib.h"
-
+#include "Gamepad.h"
 
 class ReboundRumbleRobot : public IterativeRobot
 {
-
-		
+	static const int LEFT_DRIVE_PWM = 1; //TODO: officially figure this out
+	static const int RIGHT_DRIVE_PWM = 2; //TODO: officially figure this out
+				
+	Victor * left_drive;
+	Victor * right_drive;
+	RobotDrive * drive;
+	
+	Gamepad * pilot;
+	
 public:
 
 	ReboundRumbleRobot(void)	{
@@ -15,7 +22,11 @@ public:
 	/********************************** Init Routines *************************************/
 
 	void RobotInit(void) {
-
+		left_drive = new Victor(LEFT_DRIVE_PWM);
+		right_drive = new Victor(RIGHT_DRIVE_PWM);
+		drive = new RobotDrive(left_drive,right_drive);
+		
+		pilot = new Gamepad(1); //we're using the logitech gamepad for this
 	}
 	
 	void DisabledInit(void) {
@@ -42,9 +53,12 @@ public:
 
 	
 	void TeleopPeriodic(void) {
-	
-
-	} // TeleopPeriodic(void)
+		float left_value = pilot->GetLeftY();
+		float right_value = pilot->GetRightY();
+		
+		drive->TankDrive(left_value, right_value);
+				
+	}
 
 
 
