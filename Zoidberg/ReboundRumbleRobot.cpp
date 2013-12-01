32f796d89071653ae16f88f3ded0b419174a21d7
ratpack <scottemorton@comcast.net>
2013-12-01 12:44:29 -0500

    blank robot code

---
+++ Zoidberg/ReboundRumbleRobot.cpp
@@ -0,0 +1,53 @@
+#include "WPILib.h"
+
+
+class ReboundRumbleRobot : public IterativeRobot
+{
+
+		
+public:
+
+	ReboundRumbleRobot(void)	{
+		
+	}
+	
+	
+	/********************************** Init Routines *************************************/
+
+	void RobotInit(void) {
+
+	}
+	
+	void DisabledInit(void) {
+
+	}
+
+	void AutonomousInit(void) {
+
+	}
+
+	void TeleopInit(void) {
+
+	}
+
+	/********************************** Periodic Routines *************************************/
+	
+	void DisabledPeriodic(void)  {
+
+	}
+
+	void AutonomousPeriodic(void) {
+
+	}
+
+	
+	void TeleopPeriodic(void) {
+	
+
+	} // TeleopPeriodic(void)
+
+
+
+};
+
+START_ROBOT_CLASS(ReboundRumbleRobot);
