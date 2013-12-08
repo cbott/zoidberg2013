cc57e85dab9037ce8cd87fb572b1f392500a45fb
ratpack <scottemorton@comcast.net>
2013-12-08 12:29:08 -0500

    driving works

--- Zoidberg/ReboundRumbleRobot.cpp
+++ Zoidberg/ReboundRumbleRobot.cpp
@@ -1,16 +1,26 @@
 #include "WPILib.h"
 #include "Gamepad.h"
+#include <cmath>
 
 class ReboundRumbleRobot : public IterativeRobot
 {
-	static const int LEFT_DRIVE_PWM = 1; //TODO: officially figure this out
-	static const int RIGHT_DRIVE_PWM = 2; //TODO: officially figure this out
+	static const int LEFT_DRIVE_PWM = 1;
+	static const int RIGHT_DRIVE_PWM = 2;
 				
 	Victor * left_drive;
 	Victor * right_drive;
 	RobotDrive * drive;
 	
 	Gamepad * pilot;
+	
+	float zero(float input, float min) {
+		if (abs(input) < min){
+			return 0.0f;
+		} else {
+			return input;
+		}
+	}
+	
 	
 public:
 
@@ -25,6 +35,9 @@ public:
 		left_drive = new Victor(LEFT_DRIVE_PWM);
 		right_drive = new Victor(RIGHT_DRIVE_PWM);
 		drive = new RobotDrive(left_drive,right_drive);
+		//the right side of the drive train is reversed
+		drive->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
+		drive->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
 		
 		pilot = new Gamepad(1); //we're using the logitech gamepad for this
 	}
@@ -53,8 +66,8 @@ public:
 
 	
 	void TeleopPeriodic(void) {
-		float left_value = pilot->GetLeftY();
-		float right_value = pilot->GetRightY();
+		float left_value = zero(pilot->GetLeftY(), 0.05f);
+		float right_value = zero(pilot->GetRightY(), 0.05f);
 		
 		drive->TankDrive(left_value, right_value);
 				
