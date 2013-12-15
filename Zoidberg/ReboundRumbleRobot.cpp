696830c90a04ec26b04fc0e65e87e1bbef58118e
ratpack <scottemorton@comcast.net>
2013-12-15 14:24:23 -0500

    fixed drive train, changed button layout

--- Zoidberg/ReboundRumbleRobot.cpp
+++ Zoidberg/ReboundRumbleRobot.cpp
@@ -11,6 +11,15 @@ class ReboundRumbleRobot : public IterativeRobot
 	static const int ELEVATION_PWM = 5;
 	static const int PICK_UP_PWM = 6;
 	
+	//buttons
+	static const int SHOOT_BUTTON = 8;
+	static const int ELEVATE_BUTTON = 6;
+	static const int STORE_BUTTON = 7;
+	static const int PUT_DOWN_BUTTON = 5;
+	
+	
+	
+	
 	Victor * left_drive;
 	Victor * right_drive;
 	Victor * elevation;
@@ -47,6 +56,7 @@ public:
 		left_shooter = new Victor(LEFT_SHOOTER_PWM);
 		right_shooter = new Victor(RIGHT_SHOOTER_PWM);
 		pick_up = new Victor (PICK_UP_PWM);
+		 
 		
 		pilot = new Gamepad(1); //we're using the logitech gamepad for this
 	}
@@ -75,19 +85,28 @@ public:
 
 	
 	void TeleopPeriodic(void) {
-		float left_value = (zero(pilot->GetLeftY(), 0.05f))/2;
-		float right_value = (zero(pilot->GetRightX(), 0.05f))/2;
+		//drive
 		
+		float left_value = -(zero(pilot->GetLeftY(), 0.05f))/1.5;
+		float right_value = (zero(pilot->GetRightX(), 0.05f))/1.5;
 		drive->ArcadeDrive(left_value, right_value);
-		
-		bool elevating = pilot->GetNumberedButton(2);
+		/*
+		float left_value = -(zero(pilot->GetLeftY(), 0.05f))/1.5;
+		float right_value = -(zero(pilot->GetRightY(), 0.05f))/1.5;
+		drive->TankDrive(left_value, right_value);
+		*/
+		//pickup and elevate
+		bool elevating = pilot->GetNumberedButton(ELEVATE_BUTTON);
 		if (elevating){
 			elevation->Set(.5);
+			pick_up->Set(1.0);
 		}else{
 			elevation->Set(0);
+			pick_up->Set(0);
 		}
 		
-		bool shooting = pilot->GetNumberedButton(8);
+		//shooting
+		bool shooting = pilot->GetNumberedButton(SHOOT_BUTTON);
 		if (shooting){
 			left_shooter->Set(-1);
 			right_shooter->Set(-1);
@@ -95,20 +114,19 @@ public:
 			left_shooter->Set(0.000001);
 			right_shooter->Set(0.000001);
 		}
-		
-		bool pickingup = pilot->GetNumberedButton(3);
-		if (pickingup){
-			pick_up->Set(1);
-		}else{
-			pick_up->Set(0.000001);
-		}
-		
-		bool pushingdown = pilot->GetNumberedButton(7);
+		//Storing
+		bool pushingdown = pilot->GetNumberedButton(STORE_BUTTON);
 		if (pushingdown){
 			elevation->Set(-1);
-			left_shooter->Set(1);
-			right_shooter->Set(1);
+			left_shooter->Set(0.5);
+			right_shooter->Set(0.5);
 			pick_up->Set(1);
+		}
+		
+		//reversing the pickup motor
+		bool reversing = pilot->GetNumberedButton(PUT_DOWN_BUTTON);
+		if (reversing){
+			pick_up->Set(-0.5);
 		}
 	}
 
